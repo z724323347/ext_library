fac5518dcdd2bb88d89ee7b633c6792493baa43e
summer <>
2025-11-07 16:28:42 +0800

    feat: [ext] svgicon

--- lib/ui/svg/svg_icon.dart
+++ lib/ui/svg/svg_icon.dart
@@ -1,8 +1,14 @@
-// ignore_for_file: provide_deprecation_message
+// ignore_for_file: provide_deprecation_message, use_super_parameters
+
+import 'dart:io';
 
 import 'package:ext_library/lib_ext.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+
+Widget _errSvgIcon({double? width, double? height, BoxFit? fit}) {
+  return const SizedBox.shrink();
+}
 
 class SvgIcon extends SvgPicture {
   ///asset  SvgPicture  重写
@@ -13,8 +19,15 @@ class SvgIcon extends SvgPicture {
     super.height,
     super.colorFilter,
     BoxFit? fit,
+    String? package,
     @deprecated super.color,
-  }) : super.asset(assetName, fit: fit ?? BoxFit.contain);
+  }) : super.asset(
+          assetName,
+          fit: fit ?? BoxFit.contain,
+          package: package,
+          errorBuilder: (_, err, st) =>
+              _errSvgIcon(width: width, height: height, fit: fit),
+        );
 
   /// 网络图片
   SvgIcon.network(
@@ -25,9 +38,14 @@ class SvgIcon extends SvgPicture {
     super.colorFilter,
     BoxFit? fit,
     @deprecated super.color,
-  }) : super.network(url, fit: fit ?? BoxFit.contain);
+  }) : super.network(
+          url,
+          fit: fit ?? BoxFit.contain,
+          errorBuilder: (_, err, st) =>
+              _errSvgIcon(width: width, height: height, fit: fit),
+        );
 
-  /// return svg icon
+  /// return svg/image icon
   static Widget icon(
     String svgPath, {
     Key? key,
@@ -36,8 +54,44 @@ class SvgIcon extends SvgPicture {
     ColorFilter? colorFilter,
     BoxFit? fit,
     Color? color,
+    String? package,
   }) {
-    if (svgPath.isnull) {
+    Widget view = Container();
+    switch (svgPath.fileSuffix.toLower) {
+      case 'svg':
+        view = _svgView(
+          svgPath,
+          width: width,
+          height: height,
+          color: color,
+          colorFilter: colorFilter,
+          fit: fit,
+        );
+        break;
+      default:
+        view = _imageView(
+          svgPath,
+          width: width,
+          height: height,
+          color: color,
+          fit: fit,
+          package: package,
+        );
+        break;
+    }
+    return view;
+  }
+
+  static Widget _svgView(
+    String svgPath, {
+    Key? key,
+    double? width,
+    double? height,
+    ColorFilter? colorFilter,
+    BoxFit? fit,
+    Color? color,
+  }) {
+    if (svgPath.isHttp) {
       return SvgIcon.network(
         svgPath,
         key: key,
@@ -47,7 +101,7 @@ class SvgIcon extends SvgPicture {
         colorFilter: colorFilter,
       );
     }
-    if (svgPath.startsWith('assets/')) {
+    if (svgPath.isAssetR) {
       return SvgIcon.asset(
         svgPath,
         key: key,
@@ -55,6 +109,7 @@ class SvgIcon extends SvgPicture {
         height: height,
         color: color,
         colorFilter: colorFilter,
+        fit: fit,
       );
     }
     return SvgPicture.string(
@@ -65,5 +120,54 @@ class SvgIcon extends SvgPicture {
       color: color,
       colorFilter: colorFilter,
     );
+  }
+
+  static Widget _imageView(
+    String svgPath, {
+    Key? key,
+    double? width,
+    double? height,
+    BoxFit? fit,
+    Color? color,
+    String? package,
+  }) {
+    if (svgPath.isHttp) {
+      return Image.network(
+        svgPath,
+        key: key,
+        width: width,
+        height: height,
+        color: color,
+        fit: fit,
+        errorBuilder: (_, err, st) =>
+            _errSvgIcon(width: width, height: height, fit: fit),
+      );
+    }
+    if (svgPath.isAssetR) {
+      return Image.asset(
+        svgPath,
+        key: key,
+        width: width,
+        height: height,
+        color: color,
+        fit: fit,
+        package: package,
+        errorBuilder: (_, err, st) =>
+            _errSvgIcon(width: width, height: height, fit: fit),
+      );
+    }
+    if (svgPath.isFilePath) {
+      return Image.file(
+        File(svgPath),
+        key: key,
+        width: width,
+        height: height,
+        color: color,
+        fit: fit,
+        errorBuilder: (_, err, st) =>
+            _errSvgIcon(width: width, height: height, fit: fit),
+      );
+    }
+    return Container();
   }
 }
